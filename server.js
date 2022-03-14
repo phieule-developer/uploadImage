2b735db9896978c2cd139c3199ae1b7a4d661c81
Phiêu Lê Văn <levanphieu99@gmail.com>
2022-03-14 19:58:02 +0700

    fix

--- server.js
+++ server.js
@@ -17,63 +17,63 @@ var storage = multer.diskStorage({
   }
 })
 var upload = multer({ storage: storage });
-cron.schedule('0 3 * * *', async () =>{
+cron.schedule('0 3 * * *', async () => {
   fsExtra.emptyDirSync("./uploads")
 
-},{timezone:"Asia/Ho_Chi_Minh"});
-app.use(express.static(__dirname+"/uploads"));
-app.get('/uploads/:id',(req,res)=>{
-    try {
-        res.sendFile(__dirname+"/uploads/"+req.params.id);
-    } catch (error) {
+}, { timezone: "Asia/Ho_Chi_Minh" });
+app.use(express.static(__dirname + "/uploads"));
+app.get('/uploads/:id', (req, res) => {
+  try {
+    res.sendFile(__dirname + "/uploads/" + req.params.id);
+  } catch (error) {
+    return res.send({
+      success: false
+    });
+  }
+})
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  try {
+    if (!req.file) {
       return res.send({
         success: false
       });
-    }
-})
-app.post('/api/upload', upload.single('file'), (req, res) => {
-    try {
-        if (!req.file) {
-            return res.send({
-              success: false
-            });
-            
-          } else {
-            return res.send({
-              success: true,
-              link_image:`http://${req.headers.host}/uploads/${req.file.filename}`,
-            })
-          }
-    } catch (error) {
-        return res.send({
-            success: false
-          });
-    }
-    
-  });
-  app.post("/api/upload/multiple",upload.array('files',5) ,(req, res) => {
-    try {
-      if (!req.files) {
-          return res.send({
-            success: false
-          });
-          
-        } else {
-          let image_list = [];
-          for (const item of req.files) {
-            image_list.push(`http://${req.headers.host}/uploads/${item.filename}`);
-          }
-          return res.send({
-            success: true,
-            image_list
-          })
-        }
-  } catch (error) {
+
+    } else {
       return res.send({
-          success: false
-        });
+        success: true,
+        link_image: `http://${req.headers.host}/uploads/${req.file.filename}`,
+      })
+    }
+  } catch (error) {
+    return res.send({
+      success: false
+    });
   }
-  });
-app.listen(8000,()=>{
-    console.log("Server is running at port 8000");
+
+});
+app.post("/api/upload/multiple", upload.array('files', 5), (req, res) => {
+  try {
+    if (!req.files) {
+      return res.send({
+        success: false
+      });
+
+    } else {
+      let image_list = [];
+      for (const item of req.files) {
+        image_list.push(`http://${req.headers.host}/uploads/${item.filename}`);
+      }
+      return res.send({
+        success: true,
+        image_list
+      })
+    }
+  } catch (error) {
+    return res.send({
+      success: false
+    });
+  }
+});
+app.listen(5000, () => {
+  console.log("Server is running at port 8000");
 })
